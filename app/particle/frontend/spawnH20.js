052066ad07cfe5e29c65d6b63d63880d77c4a834
Ifreke Okpokowuruk <ifreke@dhcp114-19.nersc.gov>
2015-07-28 10:52:24 -0700

    Modify spawnH20.js to increase/decrease molecules -use global variable spawncount to keep track of molecules -constantly update by looking at globaljson to increase/decrease

--- app/particle/frontend/spawnH20.js
+++ app/particle/frontend/spawnH20.js
@@ -1,30 +1,46 @@
 var spawncount = 0;
 
 pc.script.create('spawnH20', function (app) {
+    
     // Creates a new SpawnH20 instance
     var SpawnH20 = function (entity) {
         this.entity = entity;
-        this.time = 0;
+        //this.time = 0;
+        this.entities = [];
     };
 
     SpawnH20.prototype = {
-        // Called once after all resources are loaded and before the first update
+        // Called once after all resources are loaded and before the first update    
         initialize: function () {
             this.moleculeTemplate = app.root.findByName("H20 Molecule");
+            for (i = 0; i < app.root.findByName("Comm").script.ajax.length(); i++){
+                this.spawn_molecule(spawncount);
+                spawncount += 1;
+            }
         },
 
         // Called every frame, dt is time in seconds since last update
         update: function (dt) {
-            this.time+=dt;
-            if(this.time > 0.05){
-                this.spawnMol(spawncount);
-                this.time=0;
-                spawncount += 1;
+            //this.time+=dt;
+            if(app.root.findByName("Comm").script.ajax.length() > spawncount){
+                var spawn_increase = spawncount;
+                for (i = spawn_increase; i < app.root.findByName("Comm").script.ajax.length(); i++){
+                    this.spawn_molecule(spawncount);
+                    spawncount += 1;
+                }
             }
             
+            if(app.root.findByName("Comm").script.ajax.length() < spawncount){
+                var spawn_decrease = spawncount;
+                for (i = spawn_decrease; i > app.root.findByName("Comm").script.ajax.length(); i--){
+                    SpawnH20.entities[i-1].entity.destroy();
+                    SpawnH20.entities.splice(i-1, 1);
+                    spawncount -= 1;
+                }
+            }
         },
         
-        spawnMol: function(num){
+        spawn_molecule: function(num){
             var e = this.moleculeTemplate.clone();
             e.setPosition(this.entity.getPosition());
             e.translate(pc.math.random(-25,25),0,pc.math.random(-25,25));
@@ -35,6 +51,7 @@ pc.script.create('spawnH20', function (app) {
             //console.log("E: ", e);
             e.name = text; 
             app.root.addChild(e);
+            SpawnH20.entities.push({ entity: e });
         }
     };
 
